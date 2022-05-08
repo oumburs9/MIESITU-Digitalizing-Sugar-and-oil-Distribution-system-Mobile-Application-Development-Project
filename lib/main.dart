6899f95565636e5815f908e913515c267a288a47
oumburs9 <oumburs9mry@gmail.com>
2022-05-08 22:52:29 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Miesitu());
-}
+void main() => runApp(Miesitu());
 
 class Miesitu extends StatelessWidget {
-  const Miesitu({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("MIESITU"),
-      ),
-      body: const Center(
-        child: Text("Miesitu"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("MIESITU"),
+        ),
+        body: const Center(
+          child: Text("Miesitu"),
+        ),
       ),
     );
   }
