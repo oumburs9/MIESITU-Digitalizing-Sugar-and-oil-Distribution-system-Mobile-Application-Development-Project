918b2346a1f72b514d8d5a3e93d5c77d30e81af4
Segnicho <segnidesta60@gmail.com>
2022-06-05 12:16:48 +0300

    adding backend

---
+++ miesitu-backend/base/serializers.py
@@ -0,0 +1,245 @@
+# from tkinter.ttk import Style
+from numpy import product
+from .models import Announcement, Customer, Feedback, Products
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+
+class CustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Products
+        fields = '__all__'
+class AnnouncementSerializer(ModelSerializer):
+    class Meta:
+        model = Announcement
+        fields = '__all__'
+        
+class FeedbackSerializer(ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
+        
+
+# class UserRegistrationSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(style = {'input_type':'password'}, write_only = True)
+#     class Meta:
+#         model = Customer
+#         fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+#         extra_kwargs = {
+#             'password':{'write_only' :True}
+#         }
+#     def save(self):
+#         account = Customer(
+#         email = self.validated_data['email'], 
+#         # user_name = self.validated_data['user_name'] ,
+#         # first_name = self.validated_data['first_name'] ,
+#         # last_name = self.validated_data['last_name'] ,
+#         password = self.validated_data['password'] ,
+#         )
+#         password = self.validated_data['password'] 
+#         password2 = self.validated_data['password2']
+#         if password != password2:
+#             raise serializers.ValidationError({'passord':'password must match'}) 
+#         account.set_password(password)
+#         account.save()
+
+        # return account
+
+
+
+# from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+class RegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+                required=True,
+                validators=[UniqueValidator(queryset=Customer.objects.all())]
+                )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    cust_img = serializers.ImageField(required =False)
+    class Meta:
+        model = Customer
+        fields = ('username','first_name', 'last_name', 'email', 'password', 'password2','cust_img')
+        # extra_kwargs = {
+        #     'first_name': {'required': True},
+        #     'last_name': {'required': True}
+        # }
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    def create(self, validated_data):
+        user = Customer.objects.create(
+        # user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            # cust_img=validated_data['cust_img'],
+        )
+        user.set_password(validated_data['password'])
+        # user.set_id(validated_data['id'])
+        user.save()
+        return user
+
+
+
+
+
+
+
+
+
+
+
+
+# String _baseUrl = "http://localhost:8080/api/v1/tutorials/";
+
+# http.Client client = InterceptedClient.build(interceptors: [
+#   TokenInterceptor(),
+# ]);
+
+# class TutorialService{
+#   static Future<List<Tutorial>> getAllTutorials(BuildContext context) async {
+#     final response = await client.get(
+#       Uri.parse(_baseUrl+"all/c"),
+#     );
+
+#     if (response.statusCode == 200){
+#       print("Entered");
+#       Iterable response_body = jsonDecode(response.body);
+#       List<Tutorial> tutorialList = List<Tutorial>.from(
+#           response_body.map((tutorialItem) => Tutorial.fromJson(tutorialItem)));
+#       print(tutorialList);
+#       return tutorialList;
+#     } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	}
+#     return [];
+#   }
+  
+#    static Future<List<Tutorial>> getEnrolledTutorials(BuildContext context) async {
+# 	print("trying to fetch enrolled tutorials");
+#     final response = await client.get(
+#       Uri.parse(_baseUrl+"enrolled"),
+#     );
+	
+# 	print("enrolled tutorials fetched with statusCode ${response.statusCode}");
+
+#     if (response.statusCode == 200){
+#       Iterable response_body = jsonDecode(response.body);
+#       List<Tutorial> tutorialList = List<Tutorial>.from(
+#           response_body.map((tutorialItem) => Tutorial.fromJson(tutorialItem)));
+#       print(tutorialList);
+#       return tutorialList;
+#     } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	}
+#     return [];
+#   }
+  
+#   static Future<List<Tutorial>> getMyTutorials(BuildContext context) async {
+# 	print("trying to fetch my tutorials");
+#     final response = await client.get(
+#       Uri.parse(_baseUrl+"mytutorials"),
+#     );
+# 	print("My tutorials fetched with statusCode ${response.statusCode}");
+#     if (response.statusCode == 200){
+#       print("my tutorials 200 status code");
+#       Iterable response_body = jsonDecode(response.body);
+#       List<Tutorial> tutorialList = List<Tutorial>.from(
+#           response_body.map((tutorialItem) => Tutorial.fromJson(tutorialItem)));
+#       print(tutorialList);
+#       return tutorialList;
+#     } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	}
+#     return [];
+#   }
+
+#   static Future<bool> createTutorial(
+#       {required String title,
+#         required String content,
+#         required String projectTitle,
+#         required String problemStatement, required BuildContext context}) async {
+#     try {
+#       final response = await client.post(
+#         Uri.parse(_baseUrl),
+#         body: jsonEncode(<String, dynamic>{
+#           'title': title,
+#           'content': content,
+#           'project': {
+#             'title': projectTitle,
+#             'problemStatement': problemStatement,
+#           }
+#         }),
+#       );
+
+#       if (response.statusCode == 201) {
+#         return true;
+#       } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	  }
+#       return false;
+#     } catch (e) {
+#       print(e);
+#       return false;
+#     }
+#   }
+
+#   static Future<bool> updateTutorial(
+#       {required String title,
+#         required String content,
+#         required String projectTitle,
+#         required String problemStatement,
+#         required int tutorialId, required  BuildContext context}) async {
+#     try {
+# 	  print("trying to update Tutorial");
+#       final response = await client.post(
+#         Uri.parse(_baseUrl + tutorialId.toString()),
+#         body: jsonEncode(<String, dynamic?>{
+#           'title': title,
+#           'content': content,
+#           'project': {
+#             'title': projectTitle,
+#             'problemStatement': problemStatement,
+#           }
+#         }),
+#       );
+# 	  print("Tutorial Update terminated with statusCode ${response.statusCode}");
+#       if (response.statusCode == 201) {
+#         return true;
+#       } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	  }
+#       return false;
+#     } catch (e) {
+#       print(e);
+#       return false;
+#     }
+#   }
+
+#   static Future<bool> deleteTutorial({required int tutorialId, required BuildContext context}) async {
+#     try {
+#       final response = await client.delete(
+#         Uri.parse(_baseUrl + tutorialId.toString()),
+#       );
+
+#       if (response.statusCode == 204) {
+#         return true;
+#       } else if (response.statusCode == 403){
+# 	    Provider.of<AppStateManager>(context, listen:false).logout();
+# 	  }
+#       return false;
+#     } catch (e) {
+#       print(e);
+#       return false;
+#     }
+#   }
+
+# }
---
+++ miesitu-backend/base/views.py
@@ -0,0 +1,285 @@
+# from ast import Return
+from lib2to3.pgen2 import token
+from django.forms import ValidationError
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from django.views import generic
+from rest_framework.views import APIView
+from base.forms import  UserRegistrationForm
+
+from base.models import Announcement, Customer, Feedback, Products
+from base.serializers import AnnouncementSerializer, CustomerSerializer, FeedbackSerializer, ProductSerializer, RegistrationSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status, generics, permissions
+from rest_framework.exceptions import AuthenticationFailed 
+class ProductList(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = permissions.IsAdminUser
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    # permission_classes = permissions.IsAuthenticated
+
+class AnnouncementList(generics.ListCreateAPIView):
+    queryset = Announcement.objects.all()
+    serializer_class = AnnouncementSerializer
+
+class AnnouncementDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Announcement.objects.all()
+    serializer_class = AnnouncementSerializer
+class FeedbackList(generics.ListAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    # permission_classes = (permissions.IsAdminUser)
+class FeedbackCreation(generics.CreateAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
+
+class CurrentUser(generics.RetrieveUpdateDestroyAPIView): 
+    serializer_class = CustomerSerializer
+    def get_object(self):
+        user = self.request.user
+        return user
+class MyFeedbackDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    # permission_classes = (permissions.IsAuthenticated)
+# class RegisterView(APIView):
+#     def post(request, data):
+#         serializer = CustomerSerializer(data=request.data)
+#         if serializer.is_valid():
+#             print("ishfurokl")
+#             serializer.save()
+#             return Response(status=status.HTTP_200_OK)
+#             # print("vnjknskjhcnxjkzvj")
+#         # serializer.is_valid(raise_exception=True)
+#         return Response(serializer.errors)
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+class LoginView(APIView):
+    def post(self, request):       
+        email = request.data["email"]
+        password = request.data["password"]
+        user = Customer.objects.filter( email = email).first() 
+        
+        refreshtoken = RefreshToken.for_user(user)
+        accesstoken = AccessToken.for_user(user) 
+        
+        # print(accesstoken)
+        # dab = Customer.objects.filter().first()       
+        # print(dab)
+        
+        if user is None:
+            raise AuthenticationFailed('User not Found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+        # print("vjfkvndfkjdf")
+        print(user.id)
+        # print(user.user_name)
+        if user.is_superuser:        
+            return Response({
+                'id':str(user.id),
+                'type':'admin',
+                'refresh': str(refreshtoken),
+                'access': str(accesstoken),
+                'username':user.username,
+                'firstname':user.first_name,
+                'lastname':user.last_name,
+                'email':email,
+                # 'image':user.cust_img
+})
+        return Response({
+        'id':str(user.id),    
+        'type':'user',
+        'refresh':str(refreshtoken),
+        'access':str(accesstoken),
+        'username':user.username,
+        'firstname':user.first_name,
+        'lastname':user.last_name,
+        'email':user.email,
+        # 'image':user.cust_img
+  })
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+
+
+class ListUsers(generics.ListAPIView):
+    
+    model = Customer
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ListUserDetail(generics.RetrieveAPIView):
+    # model = Customer
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+
+    # authentication_classes =(permissions.IsAdminUser)
+
+# class ListUsers(APIView):
+#     """
+#     View to list all users in the system.
+
+#     * Requires token authentication.
+#     * Only admin users are able to access this view.
+#     """
+#     authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.AllowAny]
+
+#     def get(self, request, format=None):
+#         """
+#         Return a list of all users.
+#         """
+#         email = [user.email for user in Customer.objects.all()]
+#         return Response(email)
+    
+# from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.models import Token
+# from rest_framework.response import Response
+
+# class CustomAuthToken(ObtainAuthToken):
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data,
+#                                            context={'request': request})
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data['user']
+#         token = Token.objects.get(user=user)
+#         print(token.key)
+#         return Response({
+#             'token': token.key,
+#             'user_id': user.pk,
+#             'email': user.email
+#         })
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# @api_view(['POST'])
+# def registerUser(request):
+#     # form = UserRegistrationForm()
+#     print("hhhhhhhhhh")
+#     if request.method == 'POST':
+#         print("kkkkkk")
+#         account = UserRegistrationSerializer(data=request.data)
+#         if account.is_valid():
+#             print("iiiiiiiiii")
+#             # user.save()
+#             account.save()
+#             # print("useeeeeer")
+#             # user = form.save(commit=False)
+
+#             # login(request, user)
+#             return Response(status=status.HTTP_200_OK)
+#             # print("Fuckiiiiu")
+#             # return
+#         else:
+            # messages.error(request, 'An error during registration')
+
+class RegisterationView(generics.CreateAPIView):
+    # permission_classes = [permissions.AllowAny]
+    queryset = Customer.objects.all()
+    # permission_classes = [permissions.AllowAny]
+    serializer_class = RegistrationSerializer
+
+# from django.views.generic import CreateView
+
+# class SignUpView(CreateView):
+#     form_class = SignUpForm
+
+
+
+
+
+
+
+
+# @api_view(['GET', 'POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#         products = Products.objects.all()
+#         serializer = ProductSerializer(products)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
+# @api_view(['GET','PUT', 'DELETE'])
+# def product_details(request, pk, format= None):
+#     try:
+#         product = Products.objects.get(pk = pk)
+#     except Products.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = ProductSerializer(product, data=request.data)
+#         return Response(serializer.data)
+   
+#     elif request.method == 'PUT':
+#         serializer = ProductSerializer(product, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+    
+#         return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+
+    
+#     elif request.method == 'DELETE':
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+# @api_view(['GET', 'POST'])
+# def announcement_list(request):
+#     if request.method == 'GET':
+#         announcements = Announcement.objects.all()
+#         serializer = ProductSerializer(announcements)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = AnnouncementSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
+                
+# @api_view(['GET','PUT', 'DELETE'])
+# def announcement_details(request, pk, format= None):
+#     try:
+#         announcement = Announcement.objects.get(pk = pk)
+#     except Products.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = AnnouncementSerializer(announcement, data=request.data)
+#         return Response(serializer.data)
+   
+#     elif request.method == 'PUT':
+#         serializer = AnnouncementSerializer(announcement, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+    
+#         return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+#     elif request.method == 'DELETE':
+#         announcement.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+# class CustomerViewSets(viewsets.ModelViewSet):
+#     queryset = Customer.objects.all()
+#     serializer_class = CustomerSerializer
+# class ProductViewSets(viewsets.ModelViewSet):
+#     queryset = Products.objects.all()
+#     serializer_class = ProductSerializer
+# class AnnouncementViewSets(viewsets.ModelViewSet):
+#     queryset = Customer.objects.all()
+#     serializer_class = AnnouncementSerializer
+
